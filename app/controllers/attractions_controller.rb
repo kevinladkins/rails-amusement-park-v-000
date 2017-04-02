a4d0de003c38d66c4a7ec21dc178470ec2a47657
Kevin Adkins <kevinladkins@gmail.com>
2017-04-02 23:53:23 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -6,4 +6,45 @@ class AttractionsController < ApplicationController
   def show
     @attraction = Attraction.find(params[:id])
   end
+
+  def new
+    @attraction = Attraction.new
+    if !current_user.admin
+      flash[:alert] = "You are not authorized to do that"
+      redirect_to attractions_path
+    end
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+    if @attraction.save
+      redirect_to @attraction
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+    if !current_user.admin
+      flash[:alert] = "You are not authorized to do that"
+      redirect_to @attraction
+    end
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+    if @attraction.update(attraction_params)
+      redirect_to @attraction
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
+  end
+
 end
